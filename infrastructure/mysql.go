e584f865b96ad5143b386643453a0ab1fa5acd8e
Leonardo Rodriguez <tehuelche@gmail.com>
2023-03-26 21:10:17 -0500

    add mysql repository

---
+++ infrastructure/mysql.go
@@ -0,0 +1,41 @@
+package infrastructure
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+)
+
+// ConnectMysqlDB opens a connection to the MysqlDB and ensures that the db is reachable
+func ConnectMysqlDB(ctx context.Context, dsn string) (*sql.DB, error) {
+	db, _ := sql.Open("mysql", dsn)
+	return db, pingMySql(ctx, db)
+}
+
+func pingMySql(ctx context.Context, db *sql.DB) (err error) {
+	// wait until db is ready
+	for start := time.Now(); time.Since(start) < (5 * time.Second); {
+		err = db.PingContext(ctx)
+		if err == nil {
+			break
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+	return err
+}
+
+// MigrateMysqlDB runs all migrations found in the given directory against the db
+func MigrateMysqlDB(db *sql.DB, migrationsDir string) error {
+	goose.SetTableName("goose_db_version")
+	return goose.Up(db, migrationsDir)
+}
+
+// MysqlRepository struct of a mysql repository
+// Needs a specific implementation of the Repository interface for every entity
+type MysqlRepository struct {
+	DB *sql.DB
+}
---
+++ infrastructure/mysql_test.go
@@ -0,0 +1,46 @@
+package infrastructure
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tehuelche/scv-go-tools/v3/mocks"
+)
+
+// TestConnectMysqlDB_InvalidDSN checks that ConnectMysqlDB returns an error when an invalid DSN is provided
+func TestConnectMysqlDB_InvalidDSN(t *testing.T) {
+	// Arrange
+	expectedError := "missing \"=\" after \"invalid-dsn\" in connection info string\""
+
+	// Act
+	_, err := ConnectPostgresDB(context.Background(), "invalid-dsn")
+
+	// Assert
+	assert.Equal(t, expectedError, err.Error())
+}
+
+// TestPingMySql_Ok checks that pingMySql does not return an error when a valid db is received
+func TestPingMySql_Ok(t *testing.T) {
+	// Arrange
+	_, db := mocks.NewSqlDB(t)
+
+	// Act
+	err := pingMySql(context.Background(), db)
+
+	// Assert
+	assert.Nil(t, err)
+}
+
+// TestMigrateMysqlDB_NotValidDirectory checks that MigrateMysqlDB retuns an error when the given directory does not exist
+func TestMigrateMysqlDB_NotValidDirectory(t *testing.T) {
+	// Arrange
+	_, db := mocks.NewSqlDB(t)
+	expectedError := "invalid-directory directory does not exist"
+
+	// Act
+	err := MigrateMysqlDB(db, "invalid-directory")
+
+	// Assert
+	assert.Equal(t, expectedError, err.Error())
+}
